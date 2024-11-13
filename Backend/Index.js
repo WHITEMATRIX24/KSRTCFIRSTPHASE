cf45e5106a6c1741bf13a33acea00f9dbb37813d
Jacob Louis <louisjacob8@gmail.com>
2024-11-13 12:20:44 +0530

    corrected the port in server file

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,9 +1,9 @@
 // Import necessary modules
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import router from './Routers/routers.js';  // Add the .js extension
-import './DataBase/Connection.js';  // Add the .js extension
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import router from "./Routers/routers.js"; // Add the .js extension
+import "./DataBase/Connection.js"; // Add the .js extension
 
 // Configure environment variables
 dotenv.config();
@@ -11,21 +11,21 @@ dotenv.config();
 // Initialize Express server
 const server = express();
 
-// Middleware 
+// Middleware
 server.use(cors());
 server.use(express.json());
 server.use(router);
-server.use('/Uploads', express.static('./Uploads'));
+server.use("/Uploads", express.static("./Uploads"));
 
 // Define port
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 // Start the server
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Health check route
-server.get('/', (req, res) => {
-    res.status(200).send('Server Health is good, Waiting for Request...');
+server.get("/", (req, res) => {
+  res.status(200).send("Server Health is good, Waiting for Request...");
 });
