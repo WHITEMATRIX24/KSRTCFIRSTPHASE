504ca480079532f04ecd875bfc9927fae553e5ea
Jacob Louis <louisjacob8@gmail.com>
2024-11-27 12:30:14 +0530

    solve error in adding vehicles in add vehicles page

--- Frontend/src/Pages/AddVehicle/AddVehicle.jsx
+++ Frontend/src/Pages/AddVehicle/AddVehicle.jsx
@@ -1,209 +1,119 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBus, faTimes } from "@fortawesome/free-solid-svg-icons";
-import Header from "../../components/common/Header";
-import { addvehicleAPI } from "../../services/allAPI";
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBus, faTimes } from '@fortawesome/free-solid-svg-icons';
+import Header from '../../components/common/Header';
+import { addvehicleAPI } from '../../services/allAPI';
 
 const AddVehicle = () => {
   const [vehicleData, setVehicleData] = useState({
-    REGNO: "",
-    BUSNO: "",
-    ALLOTTEDDEPOT: "",
-    CLASS: "",
-    status: "in_service",
+    REGNO: '',
+    BUSNO: '',
+    ALLOTTEDDEPOT: '',
+    CLASS: '',
+    status: 'in_service',
     maintenance_history: [],
   });
 
-  // Function to reset form fields and states
-  const handleCancel = () => {
+   // Function to reset form fields and states
+   const handleCancel = () => {
     // Reset vehicle data state to empty values
     setVehicleData({
-      REGNO: "",
-      BUSNO: "",
-      ALLOTTEDDEPOT: "",
-      CLASS: "",
-      status: "in_service",
+      REGNO: '',
+      BUSNO: '',
+      ALLOTTEDDEPOT: '',
+      CLASS: '',
+      status: 'in_service',
       maintenance_history: [],
     });
     // Reset search state to empty values
-    setSearch({ vehicleClass: "", depot: "" });
+    setSearch({ vehicleClass: '', depot: '' });
     // Reset selected state to empty values
-    setSelected({ vehicleClass: "", depot: "" });
+    setSelected({ vehicleClass: '', depot: '' });
   };
 
   const [search, setSearch] = useState({
-    vehicleClass: "",
-    depot: "",
+    vehicleClass: '',
+    depot: ''
   });
   const [dropdownsOpen, setDropdownsOpen] = useState({
     vehicleClass: false,
-    depot: false,
+    depot: false
   });
   const [selected, setSelected] = useState({
-    vehicleClass: "",
-    depot: "",
+    vehicleClass: '',
+    depot: ''
   });
 
   const vehicleClasses = [
-    { name: "AC PREMIUM SF", id: "1" },
-    { name: "BB AC SEATER", id: "2" },
-    { name: "CLASS", id: "3" },
-    { name: "ELECTRIC", id: "4" },
-    { name: "EL DD", id: "5" },
-    { name: "FP", id: "6" },
-    { name: "JN AC", id: "7" },
-    { name: "JN NAC", id: "8" },
-    { name: "ORD", id: "9" },
-    { name: "S/DLX", id: "10" },
-    { name: "S/EXP", id: "11" },
-    { name: "SEATER CUM SLEEPER AC", id: "12" },
-    { name: "SEATER CUM SLEEPER NON AC", id: "13" },
-    { name: "SEMI SLEEPER", id: "14" },
-    { name: "SFP", id: "15" },
-    { name: "SP", id: "16" },
-    { name: "SSFP", id: "17" },
-    { name: "SWIFT AC SEATER", id: "18" },
-    { name: "SWIFT DLX", id: "19" },
-    { name: "SWIFT SLEEPER", id: "20" },
+    { name: 'AC PREMIUM SF', id: '1' },
+    { name: 'BB AC SEATER', id: '2' },
+    { name: 'CLASS', id: '3' },
+    { name: 'ELECTRIC', id: '4' },
+    { name: 'EL DD', id: '5' },
+    { name: 'FP', id: '6' },
+    { name: 'JN AC', id: '7' },
+    { name: 'JN NAC', id: '8' },
+    { name: 'ORD', id: '9' },
+    { name: 'S/DLX', id: '10' },
+    { name: 'S/EXP', id: '11' },
+    { name: 'SEATER CUM SLEEPER AC', id: '12' },
+    { name: 'SEATER CUM SLEEPER NON AC', id: '13' },
+    { name: 'SEMI SLEEPER', id: '14' },
+    { name: 'SFP', id: '15' },
+    { name: 'SP', id: '16' },
+    { name: 'SSFP', id: '17' },
+    { name: 'SWIFT AC SEATER', id: '18' },
+    { name: 'SWIFT DLX', id: '19' },
+    { name: 'SWIFT SLEEPER', id: '20' }
   ];
 
   const depots = [
-    "ADR",
-    "ALP",
-    "ALY",
-    "ANK",
-    "ARD",
-    "ARK",
-    "ATL",
-    "CDM",
-    "CGR",
-    "CHT",
-    "CHY",
-    "CLD",
-    "CTL",
-    "CTR",
-    "EDT",
-    "EKM",
-    "EMY",
-    "ETP",
-    "GVR",
-    "HPD",
-    "IJK",
-    "KDR",
-    "KGD",
-    "KHD",
-    "KKD",
-    "KKM",
-    "KLM",
-    "KLP",
-    "KMG",
-    "KMR",
-    "KMY",
-    "KNI",
-    "KNP",
-    "KNR",
-    "KPM",
-    "KPT",
-    "KTD",
-    "KTM",
-    "KTP",
-    "KTR",
-    "KYM",
-    "MKD",
-    "MLA",
-    "MLP",
-    "MLT",
-    "MND",
-    "MNR",
-    "MPY",
-    "MVK",
-    "MVP",
-    "NBR",
-    "NDD",
-    "NDM",
-    "NPR",
-    "NTA",
-    "PBR",
-    "PDK",
-    "PDM",
-    "PLA",
-    "PLD",
-    "PLK",
-    "PLR",
-    "PMN",
-    "PNI",
-    "PNK",
-    "PNR",
-    "PPD",
-    "PPM",
-    "PRK",
-    "PSL",
-    "PTA",
-    "PVM",
-    "PVR",
-    "RNI",
-    "SBY",
-    "TDP",
-    "TDY",
-    "TLY",
-    "TMY",
-    "TPM",
-    "TSR",
-    "TVL",
-    "TVM CL",
-    "TVM CTY",
-    "TVRA",
-    "VDA",
-    "VDY",
-    "VJD",
-    "VKB",
-    "VKM",
-    "VLD",
-    "VRD",
-    "VTR",
-    "VZM",
+    'ADR', 'ALP', 'ALY', 'ANK', 'ARD', 'ARK', 'ATL', 'CDM', 'CGR', 'CHT', 'CHY',
+    'CLD', 'CTL', 'CTR', 'EDT', 'EKM', 'EMY', 'ETP', 'GVR', 'HPD', 'IJK', 'KDR',
+    'KGD', 'KHD', 'KKD', 'KKM', 'KLM', 'KLP', 'KMG', 'KMR', 'KMY', 'KNI', 'KNP',
+    'KNR', 'KPM', 'KPT', 'KTD', 'KTM', 'KTP', 'KTR', 'KYM', 'MKD', 'MLA', 'MLP',
+    'MLT', 'MND', 'MNR', 'MPY', 'MVK', 'MVP', 'NBR', 'NDD', 'NDM', 'NPR', 'NTA',
+    'PBR', 'PDK', 'PDM', 'PLA', 'PLD', 'PLK', 'PLR', 'PMN', 'PNI', 'PNK', 'PNR',
+    'PPD', 'PPM', 'PRK', 'PSL', 'PTA', 'PVM', 'PVR', 'RNI', 'SBY', 'TDP', 'TDY',
+    'TLY', 'TMY', 'TPM', 'TSR', 'TVL', 'TVM CL', 'TVM CTY', 'TVRA', 'VDA', 'VDY',
+    'VJD', 'VKB', 'VKM', 'VLD', 'VRD', 'VTR', 'VZM'
   ];
 
+
   const handleSearchChange = (type, value) => {
-    setSearch((prevSearch) => ({ ...prevSearch, [type]: value }));
+    setSearch(prevSearch => ({ ...prevSearch, [type]: value }));
   };
 
   const handleDropdownToggle = (type) => {
-    setDropdownsOpen((prevState) => ({
-      ...prevState,
-      [type]: !prevState[type],
-    }));
+    setDropdownsOpen(prevState => ({ ...prevState, [type]: !prevState[type] }));
   };
 
   const handleBlur = (type) => {
-    setTimeout(
-      () => setDropdownsOpen((prevState) => ({ ...prevState, [type]: false })),
-      200
-    );
+    setTimeout(() => setDropdownsOpen(prevState => ({ ...prevState, [type]: false })), 200);
   };
 
   const handleSelectionChange = (type, value) => {
-    setSelected((prevSelected) => ({ ...prevSelected, [type]: value }));
-    setSearch((prevSearch) => ({ ...prevSearch, [type]: value }));
-    setDropdownsOpen((prevState) => ({ ...prevState, [type]: false }));
+    setSelected(prevSelected => ({ ...prevSelected, [type]: value }));
+    setSearch(prevSearch => ({ ...prevSearch, [type]: value }));
+    setDropdownsOpen(prevState => ({ ...prevState, [type]: false }));
 
-    if (type === "vehicleClass") {
-      setVehicleData((prevData) => ({
+    if (type === 'vehicleClass') {
+      setVehicleData(prevData => ({
         ...prevData,
-        CLASS: value,
+        CLASS: value
       }));
-    } else if (type === "depot") {
-      setVehicleData((prevData) => ({
+    } else if (type === 'depot') {
+      setVehicleData(prevData => ({
         ...prevData,
-        ALLOTTEDDEPOT: value,
+        ALLOTTEDDEPOT: value
       }));
     }
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setVehicleData((prevData) => ({ ...prevData, [name]: value }));
+    setVehicleData(prevData => ({ ...prevData, [name]: value }));
   };
 
   const handleSubmit = async (e) => {
@@ -217,29 +127,27 @@ const AddVehicle = () => {
 
       // Handle different response statuses from the server
       if (result.status === 406) {
-        alert("Vehicle already exists");
+        alert('Vehicle already exists');
       } else if (result.status === 201) {
         // Reset form state after successful submission
-        alert("Vehicle successfully added");
+        alert('Vehicle successfully added');
         setVehicleData({
-          CLASS: "",
-          BUSNO: "",
-          REGNO: "",
-          ALLOTTEDDEPOT: "",
-          status: "in_service", // Default to 'in_service' if not provided
+          CLASS: '',
+          BUSNO: '',
+          REGNO: '',
+          ALLOTTEDDEPOT: '',
+          status: 'in_service', // Default to 'in_service' if not provided
           maintenance_history: [],
         });
-        setSelected({ vehicleClass: "", depot: "" }); // Reset other related states
-        setSearch({ vehicleClass: "", depot: "" });
+        setSelected({ vehicleClass: '', depot: '' }); // Reset other related states
       } else {
-        alert("Unexpected response from server");
+        alert('Unexpected response from server');
       }
     } catch (error) {
       // Handle detailed error scenarios
       if (error.response) {
         const httpStatus = error.response.status;
-        const responseMessage =
-          error.response.data?.message || "Unexpected error occurred";
+        const responseMessage = error.response.data?.message || "Unexpected error occurred";
         console.error("Server response error:", error.response);
 
         // Display appropriate error messages based on HTTP status code
@@ -253,15 +161,15 @@ const AddVehicle = () => {
       } else {
         // Handle network errors or if error.response doesn't exist
         console.error("Network error:", error);
-        alert("Network error: Please check your connection and try again.");
+        alert('Network error: Please check your connection and try again.');
       }
     }
   };
 
-  const filteredVehicleClasses = vehicleClasses.filter((vehicleClass) =>
+  const filteredVehicleClasses = vehicleClasses.filter(vehicleClass =>
     vehicleClass.name.toLowerCase().includes(search.vehicleClass.toLowerCase())
   );
-  const filteredDepots = depots.filter((depot) =>
+  const filteredDepots = depots.filter(depot =>
     depot.toLowerCase().includes(search.depot.toLowerCase())
   );
 
@@ -276,10 +184,7 @@ const AddVehicle = () => {
               <Card className="shadow-sm border-0">
                 <Card.Body>
                   <Card.Title className="d-flex align-items-center">
-                    <FontAwesomeIcon
-                      icon={faBus}
-                      className="me-2 text-primary"
-                    />
+                    <FontAwesomeIcon icon={faBus} className="me-2 text-primary" />
                     Vehicle Details
                   </Card.Title>
                   <hr className="mb-4" />
@@ -292,42 +197,24 @@ const AddVehicle = () => {
                           <Form.Label>Vehicle Class</Form.Label>
                           <Form.Control
                             value={search.vehicleClass}
-                            onChange={(e) =>
-                              handleSearchChange("vehicleClass", e.target.value)
-                            }
-                            onFocus={() => handleDropdownToggle("vehicleClass")}
-                            onBlur={() => handleBlur("vehicleClass")}
+                            onChange={(e) => handleSearchChange('vehicleClass', e.target.value)}
+                            onFocus={() => handleDropdownToggle('vehicleClass')}
+                            onBlur={() => setTimeout(()=>{handleBlur('vehicleClass')},1000)}
                             placeholder="Search or select a vehicle class"
                           />
                           {dropdownsOpen.vehicleClass && (
-                            <div
-                              style={{
-                                border: "1px solid #ddd",
-                                marginTop: "5px",
-                                padding: "10px",
-                              }}
-                            >
+                            <div style={{ border: '1px solid #ddd', marginTop: '5px', padding: '10px' }}>
                               {filteredVehicleClasses.length > 0 ? (
-                                filteredVehicleClasses.map(
-                                  (vehicleClass, index) => (
-                                    <Form.Check
-                                      type="radio"
-                                      label={vehicleClass.name}
-                                      value={vehicleClass.name}
-                                      checked={
-                                        selected.vehicleClass ===
-                                        vehicleClass.name
-                                      }
-                                      onChange={() =>
-                                        handleSelectionChange(
-                                          "vehicleClass",
-                                          vehicleClass.name
-                                        )
-                                      }
-                                      key={index}
-                                    />
-                                  )
-                                )
+                                filteredVehicleClasses.map((vehicleClass, index) => (
+                                  <Form.Check
+                                    type="radio"
+                                    label={vehicleClass.name}
+                                    value={vehicleClass.name}
+                                    checked={selected.vehicleClass === vehicleClass.name}
+                                    onChange={() => handleSelectionChange('vehicleClass', vehicleClass.name)}
+                                    key={index}
+                                  />
+                                ))
                               ) : (
                                 <div>No matching vehicle classes found</div>
                               )}
@@ -361,21 +248,13 @@ const AddVehicle = () => {
                           <Form.Label>Depot</Form.Label>
                           <Form.Control
                             value={search.depot}
-                            onChange={(e) =>
-                              handleSearchChange("depot", e.target.value)
-                            }
-                            onFocus={() => handleDropdownToggle("depot")}
-                            onBlur={() => handleBlur("depot")}
+                            onChange={(e) => handleSearchChange('depot', e.target.value)}
+                            onFocus={() => handleDropdownToggle('depot')}
+                            onBlur={() => setTimeout(()=>{handleBlur('depot')},1000)}
                             placeholder="Search or select a depot"
                           />
                           {dropdownsOpen.depot && (
-                            <div
-                              style={{
-                                border: "1px solid #ddd",
-                                marginTop: "5px",
-                                padding: "10px",
-                              }}
-                            >
+                            <div style={{ border: '1px solid #ddd', marginTop: '5px', padding: '10px' }}>
                               {filteredDepots.length > 0 ? (
                                 filteredDepots.map((depot, index) => (
                                   <Form.Check
@@ -383,9 +262,7 @@ const AddVehicle = () => {
                                     label={depot}
                                     value={depot}
                                     checked={selected.depot === depot}
-                                    onChange={() =>
-                                      handleSelectionChange("depot", depot)
-                                    }
+                                    onChange={() => handleSelectionChange('depot', depot)}
                                     key={index}
                                   />
                                 ))
@@ -396,22 +273,15 @@ const AddVehicle = () => {
                           )}
                         </Form.Group>
                       </Col>
+
                     </Row>
 
                     <Row className="mt-4">
                       <Col className="d-flex justify-content-end">
-                        <Button
-                          variant="outline-secondary"
-                          onClick={handleCancel}
-                          size="sm"
-                          className="me-2"
-                        >
-                          <FontAwesomeIcon icon={faTimes} className="me-1" />{" "}
-                          Cancel
+                        <Button variant="outline-secondary" onClick={handleCancel} size="sm" className="me-2">
+                          <FontAwesomeIcon icon={faTimes} className="me-1" /> Cancel
                         </Button>
-                        <Button type="submit" variant="success" size="sm">
-                          Add
-                        </Button>
+                        <Button type="submit" variant="success" size="sm">Add</Button>
                       </Col>
                     </Row>
                   </Form>
